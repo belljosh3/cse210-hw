1698cee57b622b315a0d6ebd4f6924a4e3692332
belljosh3 <joshuabell135@gmail.com>
2024-03-02 16:53:49 -0600

    working on Learning activity 2

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Job {
+    //responsibilities: keeps track of company, job title, start year, end year
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+        
+    //behaviors: displays information in format "Job Title (company) StartYear-EndYear"
+    public void DisplayJobDetails () {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Resume {
+    //responsibilities: keeps track of person's name and a list of their jobs
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    //behaviors: displays the resume, which shows name, followed by each one of the jobs
+    public void DisplayResume() {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs) {
+            job.DisplayJobDetails();
+        }
+    }
+}
