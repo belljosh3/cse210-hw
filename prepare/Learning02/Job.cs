b1fb6dfbdce13b03501c04eb8fe15398ee4f8c9a
belljosh3 <joshuabell135@gmail.com>
2024-03-06 21:59:46 -0600

    fixed issues with Learning 02 project

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,12 +1,12 @@
-    public class Job {
-        //responsibilities: keeps track of company, job title, start year, end year
-        public string _company;
-        public string _jobTitle;
-        public int _startYear;
-        public int _endYear;
-            
-        //behaviors: displays information in format "Job Title (company) StartYear-EndYear"
-        public void DisplayJobDetails () {
-            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-        }
+public class Job {
+    //responsibilities: keeps track of company, job title, start year, end year
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+        
+    //behaviors: displays information in format "Job Title (company) StartYear-EndYear"
+    public void DisplayJobDetails () {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
+}
