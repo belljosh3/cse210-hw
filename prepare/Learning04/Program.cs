cf7cd63fd699b7460e87a5a67303d59a00f0e003
belljosh3 <joshuabell135@gmail.com>
2024-03-17 16:01:51 -0500

    Completed Learning assignment 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine($"{assignment.GetSummary()}");
+
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Factions", "7.3", "8-19");
+        Console.WriteLine($"{math.GetSummary()}");
+        Console.WriteLine($"{math.GetHomeworkList()}");
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Cause of World War II");
+        Console.WriteLine($"{writing.GetSummary()}\n{writing.GetWritingInformation()}");
     }
 }
